d620b20a3ea88e7b0d883a1b7129e270fc32e953
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-11-18 20:46:43 -0500

    Quick check to verify that my changes are in Git

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const ArticlesService = require('./articles-service')
 
 const app = express()
 
@@ -16,10 +17,36 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
+/* -------------------------------------------------------- */
+/*                         GET /                            */
+/* -------------------------------------------------------- */
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
+app.get('/articles', (req, res, next) => {
+  //res.send('All articles')
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+       res.json(articles)
+    })
+    .catch(next)
+})
+
+app.get('/articles/:article_id', (req, res, next) => {
+  //res.json({ 'requested_id': req.params.article_id, this: 'should fail' })
+  const knexInstance = req.app.get('db')
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      res.json(article)
+    })
+  .catch(next)
+})
+
+/* -------------------------------------------------------- */
+/*                 Error Handler                            */
+/* -------------------------------------------------------- */
 app.use(function errorHandler(error, req, res, next) {
     let response
     //if (process.env.NODE_ENV === 'production') {
